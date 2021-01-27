879202573db1ab8b0ced724829c2c25101583864
Kiruthika <66116348+Kiruthi-1312@users.noreply.github.com>
2021-01-27 22:38:07 +0530

    Create half_pyramid_numbers.cpp

---
+++ pyramid/half_pyramid_numbers.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int i,j,n;
+	cout<<"Enter the no of rows : ";
+	cin>>n;
+	for(i=0;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			cout<<j<<" ";
+		}
+		cout<<"\n";
+	}
+}
+
+// Output 
+//Enter the no of rows : 5
+//
+//1
+//1 2
+//1 2 3
+//1 2 3 4
+//1 2 3 4 5
